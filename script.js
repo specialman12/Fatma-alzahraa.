15f683606ec9a368e7bd102cbe88d30268f83ad6
Gylmsry.online <112378723+specialman12@users.noreply.github.com>
2024-09-03 21:44:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// استخدمت جافا سكريبت لإنشاء رسالة ترحيب
+function welcomeMessage() {
+  alert("تم عمل الموقع بكل حب من اجل مس فاطمه❤");
+}
+
+let backgroundMusic = document.getElementById('background-music');
+let toggleButton = document.getElementById('toggle-music');
+
+let isPlaying = true;
+
+function toggleMusic() {
+  if (isPlaying) {
+    backgroundMusic.pause();
+    toggleButton.innerHTML = '▶️Play Music';
+    isPlaying = false;
+  } else {
+    backgroundMusic.play();
+    toggleButton.innerHTML = '⏸Stop Music';
+    isPlaying = true;
+  }
+}
